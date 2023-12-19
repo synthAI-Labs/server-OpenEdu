a70534b78590f8cc96721e566d8e34028859f308
Himanshu-AT <aristech.official@gmail.com>
2023-12-19 11:51:09 +0530

    Get /learn/courses/:courseId Fixes #6

--- src/learn/learn.service.ts
+++ src/learn/learn.service.ts
@@ -3,9 +3,23 @@ import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class LearnService {
+
     constructor(private prisma: PrismaService) { }
 
-    getLearn() {
+    async getLearn() {
         return this.prisma.course.findMany();
     }
+
+    async getCourseById(courseId: string) {
+        const course = await this.prisma.course.findUnique({
+            where: { id: parseInt(courseId) },
+        });
+
+        if (!course) {
+            throw new ForbiddenException('Course not found');
+        }
+
+        return course;
+    }
+
 }
