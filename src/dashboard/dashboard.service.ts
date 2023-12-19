f6f70769b5f83513fe23c94099db62dea97fba8b
Himanshu-AT <aristech.official@gmail.com>
2023-12-19 21:26:33 +0530

    generate mini test for modules

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException, BadRequestException, ConflictException, UnauthorizedException } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { DashboardDto } from "./dto";
 
 @Injectable()
